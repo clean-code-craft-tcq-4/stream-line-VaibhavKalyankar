32def69e787a230f5a1a6d02163c9cb35d653a30
VaibhavKalyankar <105204227+VaibhavKalyankar@users.noreply.github.com>
2022-09-13 18:52:08 +0530

    Update BMS_DataSender.c

--- BMS_DataSender.c
+++ BMS_DataSender.c
@@ -1,1 +1,18 @@
+#include<stdio.h>
 
+DataStatus_en SendBatteryParameters(SensorData_st *SensorData,int DataSize)
+{
+  DataStatus_en FinalStatus = DATA_SENDING_FAILED ;
+  
+  if(SensorData != NULL)
+  {
+    FinalStatus = GenerateSensorData(SensorData,DataSize);
+    if(FinalStatus)
+    {
+      PrintBatteryData(SensorData,DataSize);
+      FinalStatus = DATA_SENDING_OK;
+    }
+  }
+  return(FinalStatus);
+}
+    
