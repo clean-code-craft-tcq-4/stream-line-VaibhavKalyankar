4ffefacf58c76437dd78b9671e5b6db3af3941c1
VaibhavKalyankar <105204227+VaibhavKalyankar@users.noreply.github.com>
2022-09-14 10:26:39 +0530

    Update Run_Test.c

--- Run_Test.c
+++ Run_Test.c
@@ -6,7 +6,7 @@
 void TEST_CASE_01()
 {
   SensorData_st test_01;
-  assert(SendBatteryParameters(&test_01,50) == DATA_SENDING_FAILED);
+  assert(SendBatteryParameters(NULL,50) == DATA_SENDING_FAILED);
   
 }
 int main()
